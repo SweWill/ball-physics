8dc73d3b9a5397c397e815ee9b7f8822898e8929
SweWill <119424405+SweWill@users.noreply.github.com>
2022-12-16 08:20:05 +0100

    Gaining height after bounce

--- ball_phsyics.py
+++ ball_phsyics.py
@@ -1,5 +1,26 @@
+from turtle import Screen, Turtle
 import turtle
+import time
 
-gravity = 9.82
+ball = turtle.Turtle()
+
+height = 500
+width = 500
+screen = Screen()
+screen.setup(height=height, width=width)
+
+
+gravity = -9.82
 velocity = 0
 energyLossFactor = 0.9
+
+ball.penup()
+ball.shape('circle')
+ball.color("red")
+
+while True:
+    ball.sety(ball.ycor()+velocity)
+    velocity += gravity
+    time.sleep(0.001)
+    if ball.ycor() < -height/2:
+        velocity = -velocity
